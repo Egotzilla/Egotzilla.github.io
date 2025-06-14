381a8e2b1f938ba41182c7cad95d5ebdd9e3b79f
Egotzilla <gulizarabenjapalaporn@gmail.com>
2025-06-14 17:41:40 +0700

    add Pets page and custom cursor

---
+++ petshop/loadPets.js
@@ -0,0 +1,23 @@
+const pets = [
+  { "name": "Buddy", "type": "Dog", "age": 3, "img": "img/dogs/dog01.jpg" },
+  { "name": "Kitty", "type": "Dog", "age": 4, "img": "img/dogs/dog02.jpg" },
+  { "name": "Tommy", "type": "Dog", "age": 5, "img": "img/dogs/dog03.jpg" },
+  { "name": "Whiskers", "type": "Cat", "age": 2, "img": "img/cats/cat01.jpg" },
+  { "name": "Mittens", "type": "Cat", "age": 3, "img": "img/cats/cat02.jpg" },
+  { "name": "Michelin", "type": "Cat", "age": 4, "img": "img/cats/cat03.jpg" }
+];
+
+var ap = document.getElementById("all-pets") // ap is announced variable that gonna have id = "all-pets"
+
+for (let i = 0; i < pets.length; i++) { // for to loop all pet list
+  var aPet = ap.appendChild(document.createElement("div")); // var aPet = create div to be child of ap
+  aPet.className = "pet"; // give aPet div a class name 'pet'
+
+  // inner to fill html commands in aPet div
+  aPet.innerHTML = ` 
+    <img src="${pets[i].img}" alt="${pets[i].name}">
+    <h3>${pets[i].name}</h3> 
+    <p>Type: ${pets[i].type}</p> 
+    <p>Age: ${pets[i].age} years</p> 
+    <button onclick="adoptPet()">Adopt Now</button>`;
+}
